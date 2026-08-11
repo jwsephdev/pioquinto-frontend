9c73cb62b72762abc239b1095b86c9d9f2bbb257
Jwseph <116055108+jwsephdev@users.noreply.github.com>
2026-08-11 18:03:43 +0800

    wrong file directory

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './css/App.css'
+import Header from './components/Header.jsx'
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+
+  return (
+   <>
+   <Navbar />
+   <Header />
+   </>
+  )
+}
+
+export default App
